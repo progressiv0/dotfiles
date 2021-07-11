51c6eef054d4dce7ca6e08b390fa42d653f16451
Michael.Pham@tui.com <Michael.Pham@tui.com@dehaj1cl0316121.fritz.box>
2021-07-11 16:27:16 +0200

    changes on installscripts

--- scripts/vim_install.sh
+++ scripts/vim_install.sh
@@ -2,11 +2,35 @@
 
 DOTFILE_DIR="$( cd -- "$(dirname "$0")"/.. >/dev/null 2>&1 ; pwd -P )"
 
-ln -s $DOTFILE_DIR/.vim ~/.vim
-if [ -f ~/.vimrc ] && ! [ -L ~/.vimrc ]
+echo "Check for neovim..."
+if command -v nvim &> /dev/null; then
+  echo "neovim is installed. Setup nvim configuration"
+  NVIM_DIR=~/.config/nvim
+
+  if [ -d $NVIM_DIR ] && ! [ -L $NVIM_DIR ]; then
+    echo "folder $NVIM_DIR already exists, move folder to ${$NVIM_DIR}BACKUP"
+    mv $NVIM_DIR ~/.config/nvimbackup
+  fi
+
+  if ! [ -d $NVIM_DIR ]; then
+    echo "Link $NVIM_DIR"
+    ln -s $DOTFILE_DIR/.config/nvim $NVIM_DIR
+  fi
+else
+  echo "neovim not found. Skip nvim configuration"
+fi
+
+
+if [ -f ~/.vimrc ] && ! [ -L ~/.vimrc ]; then
+  echo "folder .vimrc already exists, move file to .vimrcBACKUP"
   mv ~/.vimrc ~/.vimrcBACKUP
+fi
+
+if ! [ -f ~/.vimrc ]; then
+  echo "Link .vimrc"
   ln -s $DOTFILE_DIR/.vimrc ~/.vimrc
 fi
 
-# Install vim plugins
+echo "Install vim plugins"
 vim -c "PluginInstall" -c "q!" -c "q!"
+
