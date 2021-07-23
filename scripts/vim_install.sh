10ecfe8bf7d83feeef52bec7c0be516a66ba1d14
Michael Pham Private <mpham@dehaj1cl0316121.fritz.box>
2021-07-23 22:19:03 +0200

    some changes

--- scripts/vim_install.sh
+++ scripts/vim_install.sh
@@ -2,11 +2,15 @@
 
 DOTFILE_DIR="$( cd -- "$(dirname "$0")"/.. >/dev/null 2>&1 ; pwd -P )"
 
+echo "Link .vim directory"
 ln -s $DOTFILE_DIR/.vim ~/.vim
-if [ -f ~/.vimrc ] && ! [ -L ~/.vimrc ]
+#if [ -f ~/.vimrc ] && ! [ -L ~/.vimrc ]
+  echo ".vimrc already exists, move existing file to ~/.vimrcBACKUP"
   mv ~/.vimrc ~/.vimrcBACKUP
-  ln -s $DOTFILE_DIR/.vimrc ~/.vimrc
-fi
+#fi
 
-# Install vim plugins
+echo "Link .vimrc file"
+ln -s $DOTFILE_DIR/.vimrc ~/.vimrc
+
+echo "Install vim plugins"
 vim -c "PluginInstall" -c "q!" -c "q!"
